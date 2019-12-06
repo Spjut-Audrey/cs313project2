53074d6d719c098f44065bc9cf60483147c42275
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-12-06 16:18:19 -0500

    darn display isn't working. :/

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ var app = express();
 //make public/static stuff
 app.use(express.static("public"));
 app.use(express.json() );
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); 
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
 
 
 app.post("/calc", calc);     
